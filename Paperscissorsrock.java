a62f5f541f2c247110ac1d700bbfde0ecc2b94b3
BalconyJH <1091276703@qq.com>
2022-03-26 22:10:22 +0800

    Initial commit

---
+++ Paperscissorsrock.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Paperscissorsrock {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the 猜拳: ");
+        String uinput = scanner.nextLine();
+        int opt = switch (uinput) {
+            case "布" -> 1;
+            case "石头" -> 2;
+            case "剪刀" -> 3;
+            default -> 0;
+        };
+        //user uinput
+        Random rand = new Random();
+        int pinput = rand.nextInt(3) + 1;
+        //computer pinput
+        int result = opt - pinput;
+        switch (result) {
+            case 1, -2 -> System.out.println("电脑获胜");
+            case 2, -1 -> System.out.println("玩家获胜");
+            case 0 -> System.out.println("平局");
+            default -> System.out.println("输入错误");
+        }
+    }
+}
---
+++ com/itranswarp/sample/Demo05.java
@@ -0,0 +1,8 @@
+package com.itranswarp.sample;
+
+public class Demo05 {
+    public static void main(String[] args) {
+        //一张纸的厚度是0.08毫米，对折多少次达到珠穆朗玛峰的高度8848米。
+        
+}
+}
