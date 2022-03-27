1b91fac62dd49739eaec51e144b46f7bbc6f9fcd
BalconyJH <1091276703@qq.com>
2022-03-27 17:59:15 +0800

    homework

--- com/itranswarp/sample/Demo05.java
+++ com/itranswarp/sample/Demo05.java
@@ -1,8 +1,15 @@
 package com.itranswarp.sample;
 
-public class Demo05 {
+class demo {
     public static void main(String[] args) {
-        //一张纸的厚度是0.08毫米，对折多少次达到珠穆朗玛峰的高度8848米。
-        
-}
+        // 一张纸的厚度是0.08毫米，对折多少次达到珠穆朗玛峰的高度8848米。
+        int count = 0;
+        int end = 884800;
+        int start = 8;
+        while(start<=end) {
+            count++;
+            start *= 2;
+        }
+        System.out.println("要叠"+count+"次");
+    }
 }
---
+++ com/itranswarp/sample/T5.java
@@ -0,0 +1,28 @@
+package com.itranswarp.sample;
+
+import java.util.Scanner;
+
+public class T5 {
+//    4、猜数字游戏。
+//            （1）生成一个100以内的随机数。
+//            （2）从键盘输入一个整数。
+//            （3）根据输入的整数和随机数作比较，如果猜对了，提示“恭喜您，猜对了！”，如果不对，提示猜大了或是猜小了，接着猜数字，直到猜对为止。
+    public static void main(String[] args) {
+        int number = (int) (Math.random() * 100);
+        System.out.println("number=" + number);
+        int guess;
+        while (true) {
+            System.out.print("guess=");
+            Scanner scanner = new Scanner(System.in);
+            guess = scanner.nextInt();
+            if (guess == number) {
+                System.out.println("恭喜您，猜对了！");
+                break;
+            } else if (guess > number) {
+                System.out.println("猜大了！");
+            } else {
+                System.out.println("猜小了！");
+            }
+        }
+    }
+}
